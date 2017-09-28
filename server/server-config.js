7a12ee4dcf23ba1a34aac91943d9425302f058b0
Seth Dorris <sethc.dorris@gmail.com>
2017-09-28 14:03:19 -0600

    taking off server config for now

---
+++ server/server-config.js
@@ -0,0 +1,15 @@
+exports.production = {
+  user: "shdmplricidyct",
+  password: "7a66f7d335589b99fe5f665c5cb1aa7559c0e5c3dcf245570ba0c4c139e6fbdc",
+  database: "d3u8ql3k205jv6",
+  host: "ec2-23-23-192-242.compute-1.amazonaws.com",
+  port: 5432
+}
+
+exports.development = {
+  user: "postgres",
+  password: "Seth42276",
+  database: "fantasymx",
+  host: "localhost",
+  port: 5432
+}
